9601dcaa89572d894b91fe7ca52cd4c24d600317
haquangvinh1234 <138302498+haquangvinh1234@users.noreply.github.com>
2023-08-25 13:50:06 +0700

    Update hiddenpassword.js

--- Dashboard/js/hiddenpassword.js
+++ Dashboard/js/hiddenpassword.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 
         $(this).toggleClass('open');
           // Khi nhấn vào icon hiện mật khẩu thì Thay đổi icon kia lên
-        $(this).children('i').toggleClass('fa-eye-slash fa-eye');
+        $(this).toggleClass('fa-eye-slash fa-eye');
         
         // Nếu bản thân nó tồn tại call open thì co type=text để hiện mk
         if($(this).hasClass('open')){
