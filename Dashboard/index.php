4b768dbaa4fae297227099135ea71238db302aaa
haquangvinh1234 <138302498+haquangvinh1234@users.noreply.github.com>
2023-08-30 09:53:34 +0700

    Update index.php

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -32,14 +32,14 @@ $(function(){
             <span style="--i:10;"></span>
             <span style="--i:11;"></span>
             <span style="--i:12;"></span>
-            <span style="--i:13;"></span>
+<!--             <span style="--i:13;"></span>
             <span style="--i:14;"></span>
             <span style="--i:15;"></span>
             <span style="--i:16;"></span>
             <span style="--i:17;"></span>
             <span style="--i:18;"></span>
             <span style="--i:19;"></span>
-            <span style="--i:20;"></span>
+            <span style="--i:20;"></span> -->
         </div>
     </section>
                 <?php 
