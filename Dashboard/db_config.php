a96cb906d8d68d3fea650d5fb39575d5d53fd0b2
HoangViVan <vvh.vanhoang@gmail.com>
2023-09-13 11:30:47 +0700

    hgj

--- Dashboard/db_config.php
+++ Dashboard/db_config.php
@@ -16,8 +16,8 @@
         'xpathcaogiachosithuoc_link' => '../backend/product_link/chosithuoc_link.py',
         'xpathcaogiapharma_link' => '../backend/product_link/pharmacity_link.py',
         'xpathcaogiapharex_link' => '../backend/product_link/pharex_link.py',
-        'xpathcaogiamedigo_link' => '../backend/product_link/medigo_link.py',
+        'xpathcaogiamedigo_link' => '../backend/product_link/medigoapp_link.py',
         'xpathcaogialongchau_link' => '../backend/product_link/longchau_link.py',
-        'xpathcaogiaankhang_link' => '../backend/product_link/ankhang_link.py',
+        'xpathcaogiaankhang_link' => '../backend/product_link/ankhang.py',
     ]
 ?>
